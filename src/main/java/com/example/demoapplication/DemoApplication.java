b6deb552e8a0eb8264bccc10a92ac95e512ce464
Priyu714 <priyankarahinj714@gmail.com>
2023-08-01 16:13:51 +0530

    first commit

--- src/main/java/com/example/demoapplication/DemoApplication.java
+++ src/main/java/com/example/demoapplication/DemoApplication.java
@@ -2,12 +2,24 @@ package com.example.demoapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(DemoApplication.class, args);
+        ComponentDemo componentDemo = (ComponentDemo) applicationContext.getBean("componentDemo");
+        System.out.println(componentDemo.getValue());
     }
-
 }
+
+@Component
+class ComponentDemo {
+    public String getValue() {
+        return "Hello World";
+    }
+}
+
+
